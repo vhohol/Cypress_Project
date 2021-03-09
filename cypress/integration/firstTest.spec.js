5028c788ef39acfee81dc9493cb2810aba480a8c
Viktor Hohol <vhohol@bluecrewjobs.com>
2021-03-09 12:39:49 +0200

    Actions with elements

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,2 +1,16 @@
- <reference types="Cypress" />
+/// <reference types="Cypress" />
  
+it ('',() => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in');
+})
+
+it ('',() => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'Sign in');
+})
+
+it.only ('',() => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'SIGN IN', {matchCase: false} );
+})
